623a9b448cef40224a9cccf39179a1d150bc0847
kth4249 <desales4249@gmail.com>
2021-03-05 17:58:44 +0900

    5장 진행중

---
+++ src/main/java/com/jojoldu/book/springboot/config/auth/SecurityConfig.java
@@ -0,0 +1,34 @@
+package com.jojoldu.book.springboot.config.auth;
+
+import com.jojoldu.book.springboot.domain.user.Role;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@RequiredArgsConstructor
+@EnableWebSecurity // Spring Security 설정들을 활성화
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final CustomOAuth2UserService customOAuth2UserService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable().headers().frameOptions().disable()
+                // h2-console 화면을 사용하기 위해 해당 옵션들을 disable
+                .and()
+                    .authorizeRequests()
+                    // URL별 권한 관리를 설정하는 옵션의 시작점. 이후 antMatchers 옵션을 사용할 수 있음.
+                    .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**").permitAll()
+                    .antMatchers("/api/v1/**").hasRole(Role.USER.name())
+                    .anyRequest().authenticated()
+                .and()
+                    .logout()
+                        .logoutSuccessUrl("/")
+                .and()
+                    .oauth2Login()
+                        .userInfoEndpoint()
+                            .userService(customOAuth2UserService);
+    }
+}
