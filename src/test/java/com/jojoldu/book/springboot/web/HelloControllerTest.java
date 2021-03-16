086e66154ed1df617b27ae155a67e0e58bfa5f5a
kth4249 <desales4249@gmail.com>
2021-03-16 18:44:48 +0900

    study

--- src/test/java/com/jojoldu/book/springboot/web/HelloControllerTest.java
+++ src/test/java/com/jojoldu/book/springboot/web/HelloControllerTest.java
@@ -1,9 +1,13 @@
 package com.jojoldu.book.springboot.web;
 
+import com.jojoldu.book.springboot.config.auth.SecurityConfig;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -11,14 +15,25 @@ import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+
 @RunWith(SpringRunner.class)
-@WebMvcTest(controllers = HelloController.class)
+// @WebMvcTest는 WebSecurityConfigurerAdapter, WebMvcConfigurer를 비롯한 @ControllerAdvice, @Controller를 읽습니다.
+// 그러나 @Service, @Conponent는 스캔 대상이 아닙니다.
+// 따라서 CustomOAuth2UserService는 읽을 수가 없어 에러가 발생하는 것 -> SecurityConfig 스캔 대상에서 제거
+@WebMvcTest(controllers = HelloController.class,
+    excludeFilters = {
+        @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)
+    }
+)
 public class HelloControllerTest {
 
     @Autowired
     private MockMvc mvc;
 
+    @WithMockUser // 가짜로 인증된 사용자를 추가
     @Test
+    // 이후 다시 구동시
+    // IllegalArgumentException: JPA metamodel must not be empty! 에러 발생
     public void hello가_리턴된다() throws Exception {
         String hello = "hello";
 
@@ -27,6 +42,7 @@ public class HelloControllerTest {
                 .andExpect(content().string(hello));
     }
 
+    @WithMockUser // 가짜로 인증된 사용자를 추가
     @Test
     public void helloDto가_리턴된다() throws Exception {
         String name = "hello";
@@ -40,3 +56,5 @@ public class HelloControllerTest {
                 .andExpect(jsonPath("$.amount", is(amount)));
     }
 }
+
+
